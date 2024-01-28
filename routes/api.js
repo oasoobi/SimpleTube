70c527ededc90db35895d8ca212ec301bcad81b9
oasoby <134684352+oasoby@users.noreply.github.com>
2024-01-28 10:14:44 +0900

    added search page

--- routes/api.js
+++ routes/api.js
@@ -1,3 +1,4 @@
+
 const axios = require("axios");
 const express = require("express");
 const router = express.Router();
@@ -13,5 +14,11 @@ router.get("/suggest", async (req, res) => {
         res.status(500).json({ error: "Internal Server Error" });
     }
 });
+router.get("/search/:q/:page?", async (req, res) => {
+    const apiUrl = `https://yt.artemislena.eu/api/v1/search?q=${req.params.q}&page=${req.params.page || "1"}&region=JP`;
+    const response = await axios.get(apiUrl);
+    const data = await response.data;
+    res.json(data);
+})
 
 module.exports = router;
--- routes/search.js
+++ routes/search.js
@@ -6,16 +6,8 @@ const instance = [
 ]
 router.get("/:q/:p?", async (req, res) => {
     const keyword = req.params.q;
-    const page = req.params.p || "1" //指定がない場合、1ページ目のデータを表示.
-    // console.log(keyword);
-    // console.log(instance[0]);
-    // const getSearchResult = await axios.get(instance[0] + "search?q=" + keyword + "&page=" + page + "&region=JP");
-    // const searchResult = await getSearchResult.data;
-    // console.log(getSearchResult)
-    const searchResult = {
-        "s": null
-    }
-    res.render("search", {searchWord : keyword.replace(/\+/g, ' '), result : searchResult });
+    const page = req.params.p || "1" //指定がない場合、1ページ目のデータを表示. 
+    res.render("search", {searchWord : keyword.replace(/\+/g, ' '), apiUrl : `/api/search/${keyword}`, page : page});
 })
 
 module.exports = router;
